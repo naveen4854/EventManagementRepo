b59355391c50eb6fc40a43f12af20eebee446b69
Naveen <naveen4854@gmail.com>
2017-01-17 20:21:25 +0530

    database prtoject added

---
+++ EventManagement/EventManagement.Database/dbo/Tables/Conference.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Conference] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (50)  NOT NULL,
+    [Description]      NVARCHAR (MAX) NOT NULL,
+    [ShortDescription] NVARCHAR (500) NULL,
+    [Active]           BIT            NOT NULL,
+    CONSTRAINT [PK_Conference] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
