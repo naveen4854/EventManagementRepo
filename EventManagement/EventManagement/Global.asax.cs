a214684782a8bbc96ff7cb54863311536a1e1dd3
naveen4854 <naveen4854@gmail.com>
2017-02-15 00:57:39 +0530

    login trials

--- EventManagement/EventManagement/Global.asax.cs
+++ EventManagement/EventManagement/Global.asax.cs
@@ -37,5 +37,19 @@ namespace EventManagement
             Thread.CurrentThread.CurrentCulture = cInfo;
             Thread.CurrentThread.CurrentUICulture = cInfo;
         }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket != null && !authTicket.Expired)
+                {
+                    var roles = authTicket.UserData.Split(',');
+                    HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(new FormsIdentity(authTicket), roles);
+                }
+            }
+        }
     }
 }
