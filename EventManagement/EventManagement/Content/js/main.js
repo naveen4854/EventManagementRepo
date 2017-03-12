a969b7b430b30c1559d027955bacee49b1e2a0f6
Naveen <naveen4854@gmail.com>
2017-03-13 01:07:32 +0530

    homepage contact updated

--- EventManagement/EventManagement/Content/js/main.js
+++ EventManagement/EventManagement/Content/js/main.js
@@ -46,17 +46,34 @@ $(function ($) {
     //    offset: 100
     //});
 
+    // The latitude and longitude of your business / place
+    var position = [37.369619, -121.961291];
 
-    //	// Contact form validation
-    //	var form = $('.contact-form');
-    //	form.submit(function () {'use strict',
-    //		$this = $(this);
-    //		$.post($(this).attr('action'), function(data) {
-    //			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-    //		},'json');
-    //		return false;
-    //	});
+    function showGoogleMaps() {
 
+        var latLng = new google.maps.LatLng(position[0], position[1]);
+
+        var mapOptions = {
+            zoom: 16, // initialize zoom level - the max value is 21
+            streetViewControl: false, // hide the yellow Street View pegman
+            scaleControl: true, // allow users to zoom the Google Map
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            center: latLng
+        };
+
+        map = new google.maps.Map(document.getElementById('googlemaps'),
+            mapOptions);
+
+        // Show the default red marker at the location
+        marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', showGoogleMaps);
     //	$( window ).resize(function() {
     //		menuToggle();
     //	});
