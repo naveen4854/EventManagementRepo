f9a194b0ef9b91600ddc0bcbd3a1b90edc6b6faa
naveen4854 <naveen4854@gmail.com>
2017-03-13 11:44:43 +0530

    cms conference display names added || info bg images added

--- EventManagement/EventManagement.Models/ConferenceDTO.cs
+++ EventManagement/EventManagement.Models/ConferenceDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,16 @@ namespace EventManagement.DataModels
     public class ConferenceDTO
     {
         public int Id { get; set; }
+        [Display(Name = "Conference Name")]
         public string Name { get; set; }
+        [Display(Name = "Description")]
         public string Desc { get; set; }
+        [Display(Name = "Theme")]
         public string ShortDesc { get; set; }
         public string ShortImgUrl { get; set; }
+        [Display(Name = "Start Date")]
         public DateTime StartDt { get; set; }
+        [Display(Name = "End Date")]
         public DateTime EndDt { get; set; }
         public List<string> ImageUrls { get; set; }
         public List<TeamMemberDTO> Team { get; set; }
