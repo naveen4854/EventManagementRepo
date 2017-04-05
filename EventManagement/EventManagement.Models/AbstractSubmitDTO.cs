6bb4be56bec8f727c9530675b36b7872812d4598
Naveen <naveen4854@gmail.com>
2017-04-06 02:07:32 +0530

    abstract submit form validations

--- EventManagement/EventManagement.Models/AbstractSubmitDTO.cs
+++ EventManagement/EventManagement.Models/AbstractSubmitDTO.cs
@@ -7,18 +7,23 @@ namespace EventManagement.DataModels
     {
 
         [Display(Name= "Name")]
+        [Required(ErrorMessage = "Your must provide a Name")]
         public string SubmittedBy { get; set; }
         [Display(Name= "Email Id")]
         [Required(ErrorMessage = "Your must provide a EmailId")]
         public string EmailId { get; set; }
         public string Organisation { get; set; }
         [Display(Name="Upload Abstract")]
+        [Required(ErrorMessage = "Your must Select a File")]
         public HttpPostedFileBase DocUpload { get; set; }
         public string DocUrl { get; set; }
         [Display(Name="Interested In")]
+        [Required(ErrorMessage = "Your must Select Interest")]
         public int Category { get; set; }
         [Display(Name="Sessions")]
+        [Required(ErrorMessage = "Your must Select a Session")]
         public int Track { get; set; }
+        [Required(ErrorMessage = "Your must Select a Country")]
         public int Country { get; set; }
         public int Title { get; set; }
         [Required(ErrorMessage = "Your must provide a PhoneNumber")]
