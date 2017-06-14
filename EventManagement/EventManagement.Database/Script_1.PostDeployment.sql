7758790dcfbaf3a692aa2196838265535199fc12
Naveen <naveen4854@gmail.com>
2017-06-14 16:01:13 +0530

    post deployment added

---
+++ EventManagement/EventManagement.Database/Script_1.PostDeployment.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+USE [AppViralityTestDB]
+GO
+SET IDENTITY_INSERT [dbo].[Category] ON 
+
+INSERT [dbo].[Category] ([Id], [Name], [Description]) VALUES (1, N'Baby Products', N'babyproducts')
+INSERT [dbo].[Category] ([Id], [Name], [Description]) VALUES (2, N'Electronics', N'Electronic Appliances')
+SET IDENTITY_INSERT [dbo].[Category] OFF
+SET IDENTITY_INSERT [dbo].[User] ON 
+
+INSERT [dbo].[User] ([Id], [Name], [Password], [isActive]) VALUES (1, N'Naveen', N'naveen', 1)
+SET IDENTITY_INSERT [dbo].[User] OFF
